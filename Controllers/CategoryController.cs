165dd5c18064a8bf88114e9bfacd0283c95fbe4c
Cindy <cindy.mendes@hotmail.fr>
2023-11-16 18:14:18 +0100

    Implementing CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -18,6 +18,20 @@ namespace BookTrackingApi.Controllers
 
 
 
+        [HttpGet("GetAllCategories")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                var response = await _categoryService.GetAllCategories();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
         [HttpPost("AddCategory")]
         public async Task<IActionResult> AddChampagne([FromBody] AddCategoryDTO addCategory)
         {
@@ -32,7 +46,47 @@ namespace BookTrackingApi.Controllers
             }
         }
 
+        [HttpPut("UpdateCategory")]
+        public async Task<IActionResult> UpdateChampagne([FromBody] UpdateCategoryDTO updateCategory)
+        {
+            try
+            {
+                if (updateCategory.Id > 0)
+                {
+                    var response = await _categoryService.UpdateCategory(updateCategory);
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest("Please enter a correct Id");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
+        [HttpDelete("DeleteCategory/{categoryId}")]
+        public async Task<IActionResult> DeleteCategory(int categoryId)
+        {
+            try
+            {
+                if (categoryId > 0)
+                {
+                    var response = await _categoryService.DeleteCategory(categoryId);
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest("Please enter a correct Id");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
 
     }
