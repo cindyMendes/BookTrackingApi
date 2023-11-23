cf41c1f2127423571a20e11a827d90bb4df8e168
Cindy <cindy.mendes@hotmail.fr>
2023-11-23 20:15:50 +0100

    Modify DeleteSeries to delete a series if there is no books associated with this series

--- Services/SeriesService.cs
+++ Services/SeriesService.cs
@@ -111,30 +111,6 @@ namespace BookTrackingApi.Services
             }
         }
 
-        //public async Task<MainResponse> DeleteSeries(int seriesId)
-        //{
-        //    try
-        //    {
-        //        var existingSeries = await _dbContext.Series.Where(s => s.Id == seriesId).FirstOrDefaultAsync();
-
-        //        if (existingSeries != null)
-        //        {
-        //            _dbContext.Remove(existingSeries);
-        //            await _dbContext.SaveChangesAsync();
-
-        //            return new MainResponse { Message = "Series deleted successfully" };
-        //        }
-        //        else
-        //        {
-        //            return new MainResponse { IsSuccess = false, Message = "Series not found with this Id" };
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return new MainResponse { IsSuccess = false, Message = $"Error: {ex.Message}" };
-        //    }
-        //}
-
         public async Task<MainResponse> DeleteSeries(int seriesId)
         {
             try
